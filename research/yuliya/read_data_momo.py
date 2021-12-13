ddbaca7ccc5ec8724de644288a82cd2bc8e8620e
YM <yuliya.marchetti@jpl.nasa.gov>
2021-12-13 15:52:39 -0800

    add momo chem reader

--- research/yuliya/read_data_momo.py
+++ research/yuliya/read_data_momo.py
@@ -6,35 +6,53 @@ Created on Tue Nov 23 13:57:30 2021
 @author: marchett
 """
 
-
+import glob
 import numpy as np
 import h5py
 import netCDF4 as nc4
+from scipy.io import netcdf
 import matplotlib as mp
 from matplotlib import pyplot as plt
+import cartopy
+import cartopy.crs as ccrs
 
 
-path = '/Volumes/MLIA_active_data/data_SUDSAQ/test_data/'
-model_file = 'outputs/2hr_o3_2019_sfc.nc'
-input_file = 'inputs/2hr_t_2019_sfc.nc'
-
-nc_out = nc4.Dataset(path + model_file, 'r')
-nc_in = nc4.Dataset(path + input_file, 'r')
-
-#check variables
-nc_out.variables.keys()
-
-#time dim 4380
-time = nc_out['time'][:]
-#space dim 320 x 160
-lon = nc_out['lon'][:]
-lat = nc_out['lat'][:]
-ozone = nc_out['o3'][:]
+path = '/Volumes/MLIA_active_data/data_SUDSAQ/MOMO_test/'
+model_file = 'outputs/2hr_o3_2005_sfc.nc'
 
 
-x, y = np.array(np.meshgrid(lon, lat))
+inputs = {}
+input_files = glob.glob(path + 'inputs/*2012*')
+for d in range(len(input_files)):
+    nc_in = netcdf.netcdf_file(input_files[d],'r')
+    k = list(nc_in.variables.keys())[-1]
+
+    inputs['lon'] = nc_in.variables['lon'][:]
+    inputs['lat'] = nc_in.variables['lat'][:]
+    inputs['time'] = nc_in.variables['time'][:]
+    inputs[k] = nc_in.variables[k][:]
+    
+
+outputs = {}
+output_files = glob.glob(path + 'outputs/*_o3_2012*')
+for d in range(len(output_files)):
+    nc_out = netcdf.netcdf_file(output_files[d],'r')
+    k = list(nc_out.variables.keys())[-1]
+
+    outputs['lon'] = nc_out.variables['lon'][:]
+    outputs['lat'] = nc_out.variables['lat'][:]
+    outputs['time'] = nc_out.variables['time'][:]
+    outputs[k] = nc_out.variables[k][:]
+
+
+x, y = np.array(np.meshgrid(outputs['lon'], outputs['lat']))
 
 plt.figure()
-plt.pcolor(x, y, np.array(ozone[0, :, :]))
+ax = plt.subplot(projection = ccrs.PlateCarree())
+plt.pcolor(x, y, np.array(outputs['o3'][0, :, :]))
+ax.coastlines()
+
+
+
 
 
