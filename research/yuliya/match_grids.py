01b5fb25431ec16c1121e054d82819ae9a236ecf
YM <yuliya.marchetti@jpl.nasa.gov>
2022-01-24 14:07:27 -0800

    momo reader and matching update

--- research/yuliya/match_grids.py
+++ research/yuliya/match_grids.py
@@ -20,72 +20,207 @@ import cartopy.feature as cfeature
 import matplotlib.pyplot as plt
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 
-import read_data_momo as momo
+from scipy import stats
+
+import read_data_momo
 
 
 data_root_dir = '/Volumes/MLIA_active_data/data_SUDSAQ/'
-momo_root_dir = '/Volumes/MLIA_active_data/data_SUDSAQ/MOMO_test/'
+momo_root_dir = '/Volumes/MLIA_active_data/data_SUDSAQ/MOMO/'
+toar_output = f'{data_root_dir}/processed/summary_dp/TOAR2/'
+momo_output = f'{data_root_dir}/processed/summary_dp/MOMO/'
 
-year = '2012'
-month = '07'
+years = ['2012']
+months = ['06', '07', '08']
+names = ['o3', 't', 'q']
+summaries = ['mean', 'std', 'count']
 
-#save
-toar_output = f'{data_root_dir}/processed/TOAR2/summary_dp'
-toar_file = f'{toar_output}/toar2_{year}_{month}.h5'
-with closing(h5py.File(toar_file, 'r')) as f:
-        lon = f['lon'][:]
-        lat = f['lat'][:]
-        toar_data = f['data'][:]
-        date = f['date'][:].astype(str)
-        station =  f['station'][:].astype(str)
-
+for year in years:
+    
+    for month in tqdm(months):
+        #save
         
+        toar_file = f'{toar_output}/toar2_{year}_{month}.h5'
+        with closing(h5py.File(toar_file, 'r')) as f:
+                lon = f['lon'][:]
+                lat = f['lat'][:]
+                toar_data = f['data'][:]
+                date = f['date'][:].astype(str)
+                station =  f['station'][:].astype(str)
         
-momo = momo.get_momo(output_var = 'o3', input_var = ['t', 'q'], 
-                year = year, month = month)
-momo_lon = np.hstack([momo['lon'], 360.]) - 180
-momo_lat = np.hstack([momo['lat'], 90.])    
+        momo_file = f'{momo_output}/momo_{year}_{month}.h5' 
+        momo = {}
+        with closing(h5py.File(momo_file, 'r')) as f:
+            keys = list(f)
+            for k in keys:
+                momo[k] = f[k][:]
+        
+        # momo = read_data_momo.main(output_var = names[0], input_var = names[1,2], 
+        #                 year = year, month = month)
+        momo_lon = np.hstack([momo['lon'], 360.]) - 180
+        momo_lat = np.hstack([momo['lat'], 90.])    
+        x, y = np.meshgrid(momo['lon'], momo['lat'])
+        
+        #mdaily daytime average
+        days = np.unique(momo['date'][:,2])
+        mean_hours = [f'{x}'.zfill(2) for x in np.arange(8, 17)]
+        
+        momo_dat = []
+        momo_ud = []
+        toar_to_momo = {k: [] for k in summaries}
+        for d in days:
+            mask_d = np.in1d(momo['date'][:, 2], d)
+            mask_h = np.in1d(np.hstack(momo['hour'])[mask_d], mean_hours)
+            momo_dat.append(momo['data'][mask_d, :, :][mask_h, :, :].mean(axis = 0))
+            momo_ud.append(np.unique(momo['date'][mask_d, :][mask_h, :], axis = 0))
+        
+            mask_toar = np.in1d(date[:, 2], d)
+            # dlon = np.digitize(lon[mask_toar], momo_lon)
+            # dlat = np.digitize(lat[mask_toar], momo_lat)
+            #momo_matched = momo_da[dlat, dlon]
+            
+            for s in summaries:
+                ret = stats.binned_statistic_2d(lat[mask_toar], lon[mask_toar], 
+                                            toar_data[mask_toar], s, 
+                                            bins=[momo_lat, momo_lon],
+                                            expand_binnumbers = True)
+              
+                toar_to_momo[s].append(ret.statistic)
+            #momo_to_toar = momo_dat[-1][ret.binnumber[0], ret.binnumber[1]]
+            
 
-#monthly average
-days = np.unique(momo['date'][:,2])
-mean_hours = [f'{x}'.zfill(2) for x in np.arange(8, 17)]
-mask_h = np.in1d(np.hstack(momo['hour']), mean_hours)
-momo_dat = momo['o3'][mask_h, :, :].mean(axis = 0)
+    momo_dat = np.dstack(momo_dat)
+    #toar_to_momo = np.dstack(toar_to_momo)
+    momo_ud = np.row_stack(momo_ud)
+    toar_to_momo = {s: np.dstack(toar_to_momo[s]) for s in summaries}
+    bias = momo_dat - toar_to_momo['mean']
+   
+      
+    # new_file = f'{data_root_dir}/processed/summary_dp/momo_matched_{year}_{month}.h5' 
+    # with closing(h5py.File(new_file, 'w')) as f:
+    #     for s in summaries:
+    #         f['toar'][s] = np.dstack(toar_to_momo[s])
+    #     f['momo'] = momo_dat
+    #     f['date'] = momo_ud
+    
+    
+    cmin = np.nanmin(bias)
+    cmax = np.nanmax(bias)
+    cmax = np.min(np.abs([cmin, cmax])) *0.5
+    cmin = -cmax
+    x, y = np.meshgrid(momo['lon']-180, momo['lat'])
+    for d in range(bias.shape[2]):
+        fig = plt.figure(figsize=(18, 9))
+        ax = plt.subplot(projection = ccrs.PlateCarree())
+        #plt.contourf(lon-180, lat, (momo_dat - means), levels = 50, cmap = 'coolwarm')
+        #plt.pcolor(x, y, toar_to_momo.mean(axis = 2), cmap = 'coolwarm')
+        plt.pcolor(x, y, bias[:, :, d], cmap = 'coolwarm')
+        plt.clim((cmin, cmax))
+        plt.colorbar()
+        ax.set_global()
+        ax.coastlines()
+        gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
+                          linewidth=1, color='gray', alpha=0.5, linestyle='--')
+        gl.xformatter = LONGITUDE_FORMATTER
+        gl.yformatter = LATITUDE_FORMATTER
+        ax.stock_img()
+        plt.title(f'daily bias (mean(momo 8-4) - toar), year = {year}, month = {month}, day = {d+1}')
+        ax.set_extent([-140, -50, 10, 80], crs=ccrs.PlateCarree())
+        plt.savefig(f'{data_root_dir}/processed/plots/bias_{year}_{month}_{d}.png', 
+                    bbox_inches = 'tight')
+        plt.close()
+
+    #montly mean
+    fig = plt.figure(figsize=(18, 9))
+    ax = plt.subplot(projection = ccrs.PlateCarree())
+    #plt.contourf(lon-180, lat, (momo_dat - means), levels = 50, cmap = 'coolwarm')
+    #plt.pcolor(x, y, toar_to_momo.mean(axis = 2), cmap = 'coolwarm')
+    plt.pcolor(x, y, np.nanmean(bias, axis = 2), cmap = 'coolwarm')
+    plt.clim(cmin, cmax)
+    plt.colorbar()
+    ax.set_global()
+    ax.coastlines()
+    gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
+                      linewidth=1, color='gray', alpha=0.5, linestyle='--')
+    gl.xformatter = LONGITUDE_FORMATTER
+    gl.yformatter = LATITUDE_FORMATTER
+    ax.stock_img()
+    plt.title(f'monthly mean bias, year = {year}, month = {month}, day = {d+1}')
+    ax.set_extent([-140, -50, 10, 80], crs=ccrs.PlateCarree())
+    plt.savefig(f'{data_root_dir}/processed/plots/bias_{year}_{month}_mean.png', 
+                bbox_inches = 'tight')
+    plt.close()
+    
+    #montly std
+    fig = plt.figure(figsize=(18, 9))
+    ax = plt.subplot(projection = ccrs.PlateCarree())
+    #plt.contourf(lon-180, lat, (momo_dat - means), levels = 50, cmap = 'coolwarm')
+    #plt.pcolor(x, y, toar_to_momo.mean(axis = 2), cmap = 'coolwarm')
+    plt.pcolor(x, y, np.nanstd(bias, axis = 2), cmap = 'BuPu')
+    plt.clim(0, cmax*0.5)
+    plt.colorbar()
+    ax.set_global()
+    ax.coastlines()
+    gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
+                      linewidth=1, color='gray', alpha=0.5, linestyle='--')
+    gl.xformatter = LONGITUDE_FORMATTER
+    gl.yformatter = LATITUDE_FORMATTER
+    ax.stock_img()
+    plt.title(f'monthly mean bias, year = {year}, month = {month}, day = {d+1}')
+    ax.set_extent([-140, -50, 10, 80], crs=ccrs.PlateCarree())
+    plt.savefig(f'{data_root_dir}/processed/plots/bias_{year}_{month}_std.png', 
+                bbox_inches = 'tight')
+    plt.close()
+    
+    
+    #plot monthly average bias maps
+    # x, y = np.meshgrid(momo['lon']-180, momo['lat'])
+    # for year in years:
+    #     for month in months:
+    #         cmaps = ['jet', 'jet', 'YlOrRd']
+            
+    #         fig = plt.subplots(1, len(summaries), figsize=(18, 9))
+            
+    #         for s in range(len(summaries)):
+    #             mask = (momo_ud[:, 0] == year) & (momo_ud[:, 1] == month)
+    #             plot_array = toar_to_momo[summaries[s]].mean(axis =2)
+    #             plot_array[plot_array == 0] = np.nan
+                
+    #             #plt.subplot(1, len(summaries), d+1)
+    #             ax = plt.subplot(1, len(summaries), s+1, projection = ccrs.PlateCarree())
+    #             #plt.contourf(lon-180, lat, (momo_dat - means), levels = 50, cmap = 'coolwarm')
+    #             #plt.pcolor(x, y, toar_to_momo.mean(axis = 2), cmap = 'coolwarm')
+    #             plt.pcolor(x, y, plot_array, cmap = cmaps[s])
+    #             plt.colorbar(shrink = 0.4)
+    #             ax.set_global()
+    #             ax.coastlines()
+    #             gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
+    #                               linewidth=1, color='gray', alpha=0.5, linestyle='--')
+    #             gl.xformatter = LONGITUDE_FORMATTER
+    #             gl.yformatter = LATITUDE_FORMATTER
+    #             ax.stock_img()
+    #             plt.title(f'{summaries[s]}')
+    #             #north america
+    #             ax.set_extent([-140, -50, 10, 80], crs=ccrs.PlateCarree())
+    #             if summaries[s] == 'count':
+    #                 plt.clim((1, 10))
+    #             if summaries[s] == 'std':
+    #                 plt.clim((0, 10))    
+            
+    #         plt.tight_layout()    
+    #         #ax.set_extent([-125, -112, 30, 40], crs=ccrs.PlateCarree())
+    #         #ax.set_extent([-100, -90, 30, 40], crs=ccrs.PlateCarree())
+    #         plt.suptitle('Ozone bias (momo - toar) and stats for {month}-{yeaf}')
+    #         plt.savefig(f'{data_root_dir}/processsed/plots/momo_vs_toar_mean_{}' )
+    #         plt.close()
+
+
+
+
+
 
 
    
-counts = np.histogram2d(lon, lat, bins = [momo_lon, momo_lat])[0]
-H = np.histogram2d(lon, lat, bins = [momo_lon, momo_lat],
-                    weights = np.hstack(toar_data))[0]
-means = H.T / counts.T
-    
-#plot monthly average bias maps
-x, y = np.meshgrid(momo_lon, momo_lat)
-plt.figure(figsize=(18, 9))
-ax = plt.subplot(projection = ccrs.PlateCarree())
-#plt.contourf(lon-180, lat, (momo_dat - means), levels = 50, cmap = 'coolwarm')
-plt.pcolor(x, y, momo_dat - means, cmap = 'coolwarm')
-plt.clim([-20, 20])
-plt.colorbar()
-ax.set_global()
-ax.coastlines()
-gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
-                  linewidth=1, color='gray', alpha=0.5, linestyle='--')
-gl.xformatter = LONGITUDE_FORMATTER
-gl.yformatter = LATITUDE_FORMATTER
-ax.add_feature(cfeature.STATES)
-ax.stock_img()
-#north america
-#ax.set_extent([-140, -50, 10, 80], crs=ccrs.PlateCarree())
-ax.set_extent([-125, -112, 30, 40], crs=ccrs.PlateCarree())
-#ax.set_extent([-100, -90, 30, 40], crs=ccrs.PlateCarree())
-plt.title('Ozone bias (momo - toar) for 07-2012')
-
-
-
-    
-  
-        
    
 
 
--- research/yuliya/read_data_momo.py
+++ research/yuliya/read_data_momo.py
@@ -12,46 +12,145 @@ import h5py
 import netCDF4 as nc4
 from scipy.io import netcdf
 import matplotlib as mp
+from contextlib import closing
+from datetime import datetime, timedelta, date
 from matplotlib import pyplot as plt
+from tqdm import tqdm
 import cartopy
 import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 
 
-path = '/Volumes/MLIA_active_data/data_SUDSAQ/MOMO_test/'
-model_file = 'outputs/2hr_o3_2005_sfc.nc'
+
+def daterange(start_date, end_date):
+    delta = timedelta(hours = 2)
+    while start_date < end_date:
+        yield start_date
+        start_date += delta
 
 
-inputs = {}
-input_files = glob.glob(path + 'inputs/*2012*')
-for d in range(len(input_files)):
-    nc_in = netcdf.netcdf_file(input_files[d],'r')
-    k = list(nc_in.variables.keys())[-1]
 
-    inputs['lon'] = nc_in.variables['lon'][:]
-    inputs['lat'] = nc_in.variables['lat'][:]
-    inputs['time'] = nc_in.variables['time'][:]
-    inputs[k] = nc_in.variables[k][:]
+def main(outputs, inputs, years, months):
     
+    if inputs == 'all':
+        inputs = ['t', 'q', 'ps', 'u', 'v']
+        
+    if len(months) == 0:
+        months = [f'{x}'.zfill(2) for x in np.arange(1, 13)]
+        
+    momo_root_dir = '/Volumes/MLIA_active_data/data_SUDSAQ/MOMO/'
+    data_output_dir = '/Volumes/MLIA_active_data/data_SUDSAQ/processed/summary_dp/MOMO/'
+    
+    data_dict = {}
+    
+    for year in years:
+        print(f'year -----> {year}')
+        output_file = glob.glob(f'{momo_root_dir}/outputs/2hr_{outputs}_{year}*')[0]
+        nc_out = netcdf.netcdf_file(output_file,'r')
+        k = list(nc_out.variables.keys())[-1]
+    
+        data_dict['lon'] = nc_out.variables['lon'][:]
+        data_dict['lat'] = nc_out.variables['lat'][:]
+        momo_dat = nc_out.variables[k][:]
+        
+        inputs_dict = {}
+        if inputs is not None:
+            for v in inputs:
+                input_file = glob.glob(f'{momo_root_dir}/inputs/2hr_{v}_{year}*')[0]
+                nc_in = netcdf.netcdf_file(input_file,'r')
+                k = list(nc_in.variables.keys())[-1]
+                inputs_dict[v] = nc_in.variables[k][:]
+        
+        start_date = datetime(int(year), 1, 1)
+        end_date = datetime(int(year)+1, 1, 1)
+        dates_dt = []
+        for single_date in daterange(start_date, end_date):
+            dates_dt.append(single_date.strftime("%Y-%m-%d %H:%M"))
+        dates = np.row_stack([a.split(' ')[0].split('-') for a in dates_dt])
+        hours = np.row_stack([a.split(' ')[1].split(':')[0] for a in dates_dt])
+        
+        for month in tqdm(months):
+            # input_file = glob.glob(f'{momo_root_dir}/outputs/2hr_{outputs}_{year}*')[0]
+            # nc_in = netcdf.netcdf_file(input_file,'r')
+            # k = list(nc_in.variables.keys())[-1]
+        
+            # data_dict['lon'] = nc_in.variables['lon'][:]
+            # data_dict['lat'] = nc_in.variables['lat'][:]
+            # momo_dat = nc_in.variables[k][:]
+            
+            # start_date = datetime(int(year), 1, 1)
+            # end_date = datetime(int(year)+1, 1, 1)
+            # dates_dt = []
+            # for single_date in daterange(start_date, end_date):
+            #     dates_dt.append(single_date.strftime("%Y-%m-%d %H:%M"))
+            # dates = np.row_stack([a.split(' ')[0].split('-') for a in dates_dt])
+            # hours = np.row_stack([a.split(' ')[1].split(':')[0] for a in dates_dt])
+            
+            mask = dates[:, 1] == month
+            data_dict[outputs] = momo_dat[mask] 
+            data_dict['date'] = dates[mask].astype(np.string_)
+            data_dict['hour'] = hours[mask].astype(np.string_)
+            
+            if inputs is not None:
+                for v in inputs:
+                    data_dict[v] = inputs_dict[v][mask]
+            
+            # if inputs is not None:
+            #     for v in inputs:
+            #         input_file = glob.glob(f'{momo_root_dir}/inputs/2hr_{v}_{year}*')[0]
+            #         nc_in = netcdf.netcdf_file(input_file,'r')
+            #         k = list(nc_in.variables.keys())[-1]
+            #         data_dict[v] = nc_in.variables[k][mask]
+            
+            #save
+            ofile = f'momo_{year}_{month}.h5'
+            with closing(h5py.File(data_output_dir + ofile, 'w')) as f:
+                for k in data_dict.keys():
+                    f[k] = data_dict[k]
 
-outputs = {}
-output_files = glob.glob(path + 'outputs/*_o3_2012*')
-for d in range(len(output_files)):
-    nc_out = netcdf.netcdf_file(output_files[d],'r')
-    k = list(nc_out.variables.keys())[-1]
 
-    outputs['lon'] = nc_out.variables['lon'][:]
-    outputs['lat'] = nc_out.variables['lat'][:]
-    outputs['time'] = nc_out.variables['time'][:]
-    outputs[k] = nc_out.variables[k][:]
+    # outputs = get_momo(input_var = None, month = '07')
+    # monthly_mean = outputs['o3'].mean(axis = 0)
+    
+    # x, y = np.array(np.meshgrid(outputs['lon'], outputs['lat']))
+    # plt.figure(figsize=(18, 9))
+    # ax = plt.subplot(projection = ccrs.PlateCarree())
+    # #plt.contourf(lon-180, lat, (momo_dat - means), levels = 50, cmap = 'coolwarm')
+    # plt.pcolor(x, y, monthly_mean, cmap = 'jet')
+    # plt.colorbar()
+    # ax.set_global()
+    # ax.coastlines()
+    # gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
+    #                   linewidth=1, color='gray', alpha=0.5, linestyle='--')
+    # gl.xformatter = LONGITUDE_FORMATTER
+    # gl.yformatter = LATITUDE_FORMATTER
+    # #ax.add_feature(cfeature.STATES)
+    # ax.stock_img()
+    # #north america
+    # #ax.set_extent([-140, -50, 10, 80], crs=ccrs.PlateCarree())
+    # #ax.set_extent([-125, -112, 30, 40], crs=ccrs.PlateCarree())
+    # plt.title('Mean ozone estimate for 07-2012')
 
 
-x, y = np.array(np.meshgrid(outputs['lon'], outputs['lat']))
 
-plt.figure()
-ax = plt.subplot(projection = ccrs.PlateCarree())
-plt.pcolor(x, y, np.array(outputs['o3'][0, :, :]))
-ax.coastlines()
+if __name__ == '__main__':
+    import argparse
 
+    parser = argparse.ArgumentParser()
+    #parser.add_argument('data_root_dir', type=str)
+    parser.add_argument('--years', default = ['2012'], type=str)
+    parser.add_argument('--months', default = [], type=str)
+    parser.add_argument('--inputs', type=str, default=['t', 'q'])
+    parser.add_argument('--outputs', type=str, default='o3')
+    #parser.add_argument('out_file', type=str)
+
+    args = parser.parse_args()
+    main(**vars(args))
+    
+    
+    
+    
 
 
 
