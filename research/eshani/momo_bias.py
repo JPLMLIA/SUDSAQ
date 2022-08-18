dd8a6e0e2bde8bb61a456fd6dea54cea066413c3
Eshani J Patel <Eshani.J.Patel@jpl.nasa.gov>
2022-08-18 13:52:54 -0700

    Update momo_bias.py

--- research/eshani/momo_bias.py
+++ research/eshani/momo_bias.py
@@ -17,6 +17,7 @@ from sklearn.ensemble import RandomForestClassifier
 from numpy import asarray
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.inspection import permutation_importance
+from tqdm import tqdm #import
 
 VARS = ['o3_average_values', 'o3_daytime_avg', 'o3_nighttime_avg', 'o3_median',
         'o3_perc25', 'o3_perc75', 'o3_perc90', 'o3_perc98', 'o3_dma8eu',
@@ -38,7 +39,6 @@ NUM_FEATURES = ['alt', 'relative_alt', 'water_25km',
 CAT_FEATURES = ['climatic_zone', 'type', 'type_of_area']
 OTHER = ['id', 'country', 'htap_region', 'dataset', 'lon', 'lat']
 
-
 data = pd.read_csv('AQbench_dataset.csv')
 
 feature_names = np.array(NUM_FEATURES)
@@ -53,88 +53,60 @@ for i in x_range:
          mask = x== u
          x_cat[mask, i] = j+1
 
-
 y = np.array(data['o3_average_values']) 
 encoder = OneHotEncoder(sparse=False)
 onehot = encoder.fit_transform(data[CAT_FEATURES])
 x = np.array(np.column_stack([np.array(data[NUM_FEATURES]), onehot]))
 
-
 ds = xr.open_dataset('5_year_average.2010-2014.nc')
 
-#print(ds)
-
 ns = ds[['momo.mda8']]
-#print(ns)
 
 ns.load()
-print(ns)
-#ns = ns.to_array()
 
 coor = ns.coords
 momo_lon = ns.lon.values
 momo_lat = ns.lat.values
+momo_lon = np.hstack([momo_lon, 360])
+momo_lat = np.hstack([momo_lat, 180])
 
 mda8 = ns['momo.mda8'].values
-print(mda8)
-x, y = np.meshgrid(momo_lon, momo_lat)
+'''
+a, b = np.meshgrid(momo_lon, momo_lat)
 
-# fig, ax = plt.subplots(figsize=(18, 9),
-#                         subplot_kw={'projection': ccrs.PlateCarree()})
+fig, ax = plt.subplots(figsize=(18, 9),
+                        subplot_kw={'projection': ccrs.PlateCarree()})
 
-# ax.set_global()
-# ax.coastlines()
-# gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, 
-#                     linewidth=1, color='gray', alpha=0.5, linestyle='--')
-# gl.xformatter = LONGITUDE_FORMATTER
-# gl.yformatter = LATITUDE_FORMATTER
+ax.set_global()
+ax.coastlines()
+gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, 
+                    linewidth=1, color='gray', alpha=0.5, linestyle='--')
+gl.xformatter = LONGITUDE_FORMATTER
+gl.yformatter = LATITUDE_FORMATTER
 
-# sc = ax.pcolor(x, y, mda8, cmap="jet")
-# plt.title('5 year ozone average (actual)', fontsize=14)
-# plt.colorbar(sc)
-# plt.savefig('OzoneMap_MOMO.png')
-# plt.show()
+sc = ax.pcolor(x, y, mda8, cmap="jet")
+plt.title('5 year ozone average (actual)', fontsize=14)
+plt.colorbar(sc)
+plt.savefig('OzoneMap_MOMO.png')
+plt.show()
+'''
 
+lon = np.hstack([np.array(data['lon'])]) 
+lat = np.hstack([np.array(data['lat'])])
 
-#np.histogram2d(x, y, bins=mda8, range=None, normed=None, weights=None, density=None)
-
-lon = np.hstack([np.array(data['lon']), 360.]) + 180
-lat = np.hstack([np.array(data['lat']), 180.]) + 90
-
-
-#print out the shapes of variables and debugg, momolatlong, lat long, mda8
-
-print(lat.min())
-print(len(lat))
-print(lon.min())
-print(len(lon))
-
-
-print(len(momo_lat))
-
-print(len(momo_lon))
-
-print(mda8.shape)
-
-
-ret = stats.binned_statistic_2d(momo_lat, momo_lon,
-                            mda8, 
-                            bins=[lat, lon],
-                            expand_binnumbers = True)                    
-bias = y - ret.statistic
-# ret = stats.binned_statistic_2d(lat, lon,
-#                             y, 
-#                             bins=[momo_lat, momo_lon],
-#                             expand_binnumbers = True)                    
-# bias = mda8 - ret.statistic
-#convert to 1D array
+ret = stats.binned_statistic_2d(lat, lon,
+                            y, 
+                            bins=[momo_lat, momo_lon],
+                            expand_binnumbers = True)
+y_momo = mda8[ret.binnumber[0], ret.binnumber[1]]       
+bias = y - y_momo
 bias = bias.flatten()
 
 y = bias
 
 pred_y = np.zeros((len(y), ))
 kfold = KFold(n_splits=5, shuffle=True, random_state=1234)
-for train_index, test_index in kfold.split(x):
+for train_index, test_index in tqdm(kfold.split(x)): 
     train_x, test_x = x[train_index], x[test_index]
     train_y, test_y = y[train_index], y[test_index]
     train_mask, test_mask = mask[train_index], mask[test_index]
@@ -158,33 +130,49 @@ a, b = np.polyfit(y, pred_y, 1)
 
 plt.scatter(y, pred_y, s=3)
 plt.plot(y, a*y+b, color="red", linewidth=2)
-
-plt.title('Predicted Y vs Actual Y', fontsize=14)
-plt.xlabel('Actual Y', fontsize=14)
-plt.ylabel('Predicted Y', fontsize=14)
-
+plt.plot(y, y, color="black", linewidth=0.5)
+plt.title('Predicted Bias vs Actual Bias', fontsize=12)
+plt.xlabel('Actual Bias', fontsize=12)
+plt.ylabel('Predicted Bias', fontsize=12)
+plt.xlim([-40, 20])
+plt.ylim([-40, 20])
+plt.savefig('scatter_bias.png')
 plt.show()
-
 
 range1 = np.max(y)-np.min(y)
 range2 = np.max(pred_y)-np.min(pred_y)
- 
-plt.hist(y, bins=range1/2, color="blue", alpha=0.5)
-plt.hist(pred_y, bins=range2/2, color="red", alpha=0.5)
-plt.title('Histogram of Actual and Predicted Average Ozone over 5 years', fontsize=10)
-plt.xlabel('Average Ozone over 5 years', fontsize=10)
+
+plt.hist(y, bins=(int)(range1/2), color="blue", alpha=0.5)
+plt.hist(pred_y, bins=(int)(range2/2), color="red", alpha=0.5)
+plt.title('Histogram of Actual and Predicted Average Ozone Bias over 5 years', fontsize=12)
+plt.xlabel('Bias', fontsize=12)
 legend_drawn_flag = True
 plt.legend(["actual", "predicted"], loc=0, frameon=legend_drawn_flag)
+plt.savefig('hist_bias.png')
+plt.show()
+
+fig, ax = plt.subplots(figsize=(18, 9),
+                        subplot_kw={'projection': ccrs.PlateCarree()})
+
+ax.set_global()
+ax.coastlines()
+gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, 
+                    linewidth=1, color='gray', alpha=0.5, linestyle='--')
+gl.xformatter = LONGITUDE_FORMATTER
+gl.yformatter = LATITUDE_FORMATTER
+
+sc = ax.scatter(np.array(data['lon']), np.array(data['lat']), c=y, cmap="jet", s=3)
+plt.title('5-year ozone average bias', fontsize=12)
+plt.colorbar(sc)
+plt.savefig('OzoneMap_bias.png')
 plt.show()
 
 #Feature Importance
 
-X, y = make_classification(n_samples=5577, n_features=32, n_informative=5, n_redundant=5, random_state=1)
+X, y = make_classification(n_samples=len(y), n_features=x.shape[1], n_informative=5, n_redundant=5, random_state=1)
 model = RandomForestClassifier()
 model.fit(x, y)
 importance = model.feature_importances_
-# for i,v in enumerate(importance):
-# 	print('Feature %d: %0s, Score: %.5f' % (i, feature_names[i],v))
 normal_array = importance/np.max(importance)
 plt.bar(feature_names, normal_array, color='blue', alpha=0.5)
 
@@ -194,18 +182,14 @@ r = permutation_importance(model, x, y,
                             n_repeats=30,
                             random_state=0)
 
-# for i in range(len(r.importances_mean)):
-#     print('Feature %d: %s: %.5f +/- %.5f' % (i, feature_names[i], r.importances_mean[i], r.importances_std[i]))
-
 normal_array = r.importances_mean/np.max(r.importances_mean)
 plt.bar(feature_names, normal_array, color='red', alpha=0.5)
 
-plt.title('Feature Importance vs Permutation Importance', fontsize=14)
-plt.xlabel('Feature Name', fontsize=14)
-plt.ylabel('Normalized Importance', fontsize=14)
+plt.title('Feature Importance vs Permutation Importance of Bias', fontsize=12)
+plt.xlabel('Feature Name', fontsize=12)
+plt.ylabel('Normalized Importance', fontsize=12)
 plt.xticks(fontsize=8, rotation = 90)
 legend_drawn_flag = True
 plt.legend(["Feature Importance", "Permutation Importance"], loc=0, frameon=legend_drawn_flag)
-
+plt.savefig('importance_bias.png')
 plt.show()
-
