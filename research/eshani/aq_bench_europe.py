b8841d279b3f572d87c3a0bbc51342e8571d2a89
Eshani J Patel <Eshani.J.Patel@jpl.nasa.gov>
2022-08-19 15:32:10 -0700

    Update aq_bench_europe.py

--- research/eshani/aq_bench_europe.py
+++ research/eshani/aq_bench_europe.py
@@ -37,20 +37,20 @@ NUM_FEATURES = ['alt', 'relative_alt', 'water_25km',
 CAT_FEATURES = ['climatic_zone', 'type', 'type_of_area']
 OTHER = ['id', 'country', 'htap_region', 'dataset', 'lon', 'lat']
 
-
+# reads in csv file
 data = pd.read_csv('AQbench_dataset.csv')
 
+# filters data to only include European data stations
 lon = np.array(data['lon'])
 lat = np.array(data['lat'])
-
 mask_lon = (lon>=-25) & (lon<=65) 
 mask_lat = (lat>=35) & (lat<=72)
 mask = mask_lon & mask_lat
-
 data = data.iloc[mask]
 
 feature_names = np.array(NUM_FEATURES)
 
+# one-hot encodes the categorical vairables
 x_cat = np.zeros((data.shape[0], len(CAT_FEATURES)))
 x_range = range(len(CAT_FEATURES))
 for i in x_range:
@@ -61,12 +61,15 @@ for i in x_range:
          mask = x== u
          x_cat[mask, i] = j+1
 
-
-y = np.array(data['o3_average_values']) 
 encoder = OneHotEncoder(sparse=False)
 onehot = encoder.fit_transform(data[CAT_FEATURES])
+
+# sets y to be 5 year average ozone
+y = np.array(data['o3_average_values']) 
+# sets x to be numerical features and one-hot-encoded categorical features
 x = np.array(np.column_stack([np.array(data[NUM_FEATURES]), onehot]))
 
+# Random Forest Model ran on 5 splits of data to generate pred_y
 pred_y = np.zeros((len(y), ))
 kfold = KFold(n_splits=5, shuffle=True, random_state=1234)
 for train_index, test_index in kfold.split(x):
@@ -80,16 +83,17 @@ for train_index, test_index in kfold.split(x):
 
     pred_y[test_index] = rf_predictor.predict(test_x)
 
-#stats
+# calculates root mean squared error
 mse = sklearn.metrics.mean_squared_error(y, pred_y)
 rmse = math.sqrt(mse)
 print("Root Mean Sqaured Error = " + str(rmse))
 
+# calculates r correlation value
 r = np.corrcoef(y, pred_y)
 print("r correlation = " + str(r[0,1]))
 
+# creates scatter plot of pred_y vs y and plots line of best fit
 a, b = np.polyfit(y, pred_y, 1)
-
 plt.scatter(y, pred_y, s=3)
 plt.plot(y, a*y+b, color="red", linewidth=2)
 # plt.plot(y, y, color="black", linewidth=0.5)
@@ -101,9 +105,9 @@ plt.ylim([0, 60])
 plt.savefig('scatter_europe.png')
 plt.show()
 
+# plots histogram of y and pred_y
 range1 = np.max(y)-np.min(y)
 range2 = np.max(pred_y)-np.min(pred_y)
-
 plt.hist(y, bins=(int)(range1/2), color="blue", alpha=0.5)
 plt.hist(pred_y, bins=(int)(range2/2), color="red", alpha=0.5)
 plt.title('Histogram of Actual and Predicted Average Ozone in Europe', fontsize=12)
@@ -114,26 +118,25 @@ plt.savefig('hist_europe.png')
 plt.show()
 
 
-
-#Feature and Permutation Importance
-
+# Calculates feature importance
 X, y = make_classification(n_samples=len(y), n_features=x.shape[1], n_informative=5, n_redundant=5, random_state=1)
 model = RandomForestClassifier()
 model.fit(x, y)
 importance = model.feature_importances_
+# normalizes feature importance
 normal_array = importance/np.max(importance)
 plt.bar(feature_names, normal_array, color='blue', alpha=0.5)
 
 
-
+# Calculates permutation importance
 r = permutation_importance(model, x, y,
                             n_repeats=30,
                             random_state=0)
-
-
+# normalizes permutation importance
 normal_array = r.importances_mean/np.max(r.importances_mean)
 plt.bar(feature_names, normal_array, color='red', alpha=0.5)
 
+# plots normalized feature importance against normalized permuation importance
 plt.title('Feature Importance vs Permutation Importance of Europe', fontsize=12)
 plt.xlabel('Feature Name', fontsize=12)
 plt.ylabel('Normalized Importance', fontsize=12)
@@ -142,4 +145,3 @@ legend_drawn_flag = True
 plt.legend(["Feature Importance", "Permutation Importance"], loc=0, frameon=legend_drawn_flag)
 plt.savefig('importance_europe.png')
 plt.show()
-
