dec3c9d5cb737921cd9c6e13cb48a35f2e3d57d1
James Montgomery <jamesmo@jpl.nasa.gov>
2022-10-14 10:17:19 -0700

    Fixed hyperparameter optimization in create.py

--- research/james/hyperparamopt.py
+++ research/james/hyperparamopt.py
@@ -6,7 +6,7 @@
 from sudsaq.config import Config
 from sudsaq.data import load
 
-config = Config('research/james/create.bias.11-15.8hour_avg.yml', 'jan')
+config = Config('research/james/create.bias.11-15.8hour_avg.yml', 'dec')
 
 #%%
 
@@ -21,8 +21,8 @@ from sklearn.model_selection import (
     KFold
 )
 
-kfold  = GroupKFold(n_splits=len(set(data.time.dt.year.values)))
-groups = target.time.dt.year.values
+kfold  = GroupKFold(n_splits=len(set(t.time.dt.year.values)))
+groups = t.time.dt.year.values
 
 #%%
 gscv = GridSearchCV(
@@ -30,19 +30,41 @@ gscv = GridSearchCV(
     cv          = kfold,
     error_score = 'raise',
     n_jobs      = -1,
-    **config.hyperoptimize.GridSearchCV.param_grid
+    param_grid = {**config.hyperoptimize.GridSearchCV.param_grid}
     # **config.hyperoptimize.GridSearchCV
 )
 
 #%%
 
-gscv.fit(data, target)
+gscv.fit(d, t, groups=groups)
 
 
-{}
 
-# align_print(gscv.best_params_, enum=False, prepend='  ', print=Logger.info)
+#%%
+target.load(), data.load()
+d, t = xr.align(data.dropna('loc'), target.dropna('loc'))
+
+kfold  = GroupKFold(n_splits=len(set(t.time.dt.year.values)))
+groups = t.time.dt.year.values
+
+#%%
+from sudsaq.utils import align_print
+
+align_print(gscv.best_params_, enum=False, prepend='  ')
 
 #%%
 # Create the predictor
 model = lambda: RandomForestRegressor(**config.model.params, **gscv.best_params_)
+
+#%%
+
+for fold, (train, test) in enumerate(kfold.split(data, target, groups=groups)):
+    break
+
+#%%
+
+import xarray as xr
+
+ds = xr.open_mfdataset('/Volumes/MLIA_active_data/data_SUDSAQ/data/momo/2011/12.nc')
+ds = ds[['momo.o3', 'momo.t']]
+ds.load()
