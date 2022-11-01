4c294a6e0c183c8e02b4749df729c4974d2b3fad
James Montgomery <jamesmo@jpl.nasa.gov>
2022-11-01 13:25:40 -0700

    - Added nonversioned packages to setup.py - Added Gattaca-specific configs - Added a catch function to utils to ensure plot functions don't raise exceptions - Enabled Ray to be optional in treeinterpreter (temporary solution to Ray issues with Gattaca)

--- sudsaq/utils.py
+++ sudsaq/utils.py
@@ -235,3 +235,37 @@ def save_objects(output, kind, **others):
             )
         else:
             Logger.warning(f'Object {name!r} is not enabled to be saved in the config, skipping')
+
+def catch(func):
+    """
+    Decorator that protects the caller from an exception raised by the called function.
+
+    Parameters
+    ----------
+    func: function
+        The function to wrap
+
+    Returns
+    -------
+    _wrap: function
+        The wrapper function that calls func inside of a try/except block
+    """
+    def _wrap(*args, **kwargs):
+        """
+        Wrapper function
+
+        Parameters
+        ----------
+        *args: list
+            List of positional arguments for func
+        **kwargs: dict
+            Dict of keyword arguments for func
+        """
+        try:
+            func(*args, **kwargs)
+        except:
+            Logger.exception(f'Function <{func.__name__}> raised an exception')
+
+    # Need to pass the docs on for sphinx to generate properly
+    _wrap.__doc__ = func.__doc__
+    return _wrap
