a90e3805e5e843d9a29dd94ed0576420b8d3f2e2
Kelsey Doerksen <kelsey.doerksen@jpl.nasa.gov>
2022-12-13 14:10:56 -0800

    Fixing config

--- research/kelsey/prep_gee.py
+++ research/kelsey/prep_gee.py
@@ -6,15 +6,14 @@ matches longitude of momo, and splits into training, testing sets
 
 import xarray as xr
 
-month = 'jan'
-root_dir = '/Users/kelseydoerksen/exp_runs/rf'
-gee_dir = '/Users/kelseydoerksen/gee'
-
+month = 'jul'
+root_dir = '/Users/kelseydoerksen/exp_runs/rf/{}/all_gee_added'.format(month)
+#gee_dir = '/Users/kelseydoerksen/gee'
+gee_dir = '/Users/kelseydoerksen/exp_runs/rf/{}/all_gee_added'.format(month)
 
 def format_gee(gee, momo):
     gee = gee.expand_dims(time=momo['time'])
     gee_t = gee.transpose('lat', 'lon', 'time')
-    gee_t.coords['lon'] = gee_t.coords['lon']+180
     gee_final = gee_t.fillna(0)
     return gee_final
 
@@ -24,21 +23,26 @@ modis_data = []
 pop_data = []
 years = [2011, 2012, 2013, 2014, 2015]
 for year in years:
-    pop = xr.open_dataset('{}/pop/2010_population_density_globe.nc'.format(gee_dir))
-    modis = xr.open_dataset('{}/modis/{}_modis_globe_mode.nc'.format(gee_dir, year))
-    fire = xr.open_dataset('{}/fire/{}_fire_globe_mode.nc'.format(gee_dir, year))
-    momo = xr.open_dataset('{}/{}/{}/test.data.nc'.format(root_dir, month, year))
-    formatted_pop = format_gee(pop, momo)
+    #pop = xr.open_dataset('{}/pop/2010_population_density_globe.nc'.format(gee_dir))
+    #modis = xr.open_dataset('{}/modis/{}_modis_globe_mode.nc'.format(gee_dir, year))
+    modis = xr.open_dataset('{}/{}/{}_modis_lc_no_time.nc'.format(gee_dir, year, year))
+    #fire = xr.open_dataset('{}/fire/{}_fire_globe_mode.nc'.format(gee_dir, year))
+    #momo = xr.open_dataset('{}/{}/{}/test.data.nc'.format(root_dir, month, year))
+    momo = xr.open_dataset('{}/{}/test.data.nc'.format(root_dir, year))
+    #formatted_pop = format_gee(pop, momo)
     formatted_modis = format_gee(modis, momo)
-    formatted_fire = format_gee(fire, momo)
-    formatted_pop.to_netcdf('{}/{}/{}/test.pop.nc'.format(root_dir, month, year))
-    formatted_modis.to_netcdf('{}/{}/{}/test.modis.nc'.format(root_dir, month, year))
-    formatted_fire.to_netcdf('{}/{}/{}/test.fire.nc'.format(root_dir, month, year))
-    fire_data.append(formatted_fire)
+    #formatted_fire = format_gee(fire, momo)
+    #formatted_pop.to_netcdf('{}/{}/{}/test.pop.nc'.format(root_dir, month, year))
+    #formatted_modis.to_netcdf('{}/{}/{}/test.modis.nc'.format(root_dir, month, year))
+    formatted_modis.to_netcdf('{}/{}/test.modis.nc'.format(root_dir, year))
+    #formatted_fire.to_netcdf('{}/{}/{}/test.fire.nc'.format(root_dir, month, year))
+    #fire_data.append(formatted_fire)
     modis_data.append(formatted_modis)
-    pop_data.append(formatted_pop)
+    #pop_data.append(formatted_pop)
 
-gee_type = {'pop': pop_data, 'modis': modis_data, 'fire': fire_data}
+#gee_type = {'pop': pop_data, 'modis': modis_data, 'fire': fire_data}
+gee_type = {'modis': modis_data}
+'''
 for k, v in gee_type.items():
     train_2011 = v[1].merge(v[2]).merge(v[3]).merge(v[4])
     train_2011.to_netcdf('{}/{}/2011/train.{}.nc'.format(root_dir, month, k))
@@ -54,3 +58,19 @@ for k, v in gee_type.items():
 
     train_2015 = v[0].merge(v[1]).merge(v[2]).merge(v[3])
     train_2015.to_netcdf('{}/{}/2015/train.{}.nc'.format(root_dir, month, k))
+'''
+for k, v in gee_type.items():
+    train_2011 = v[1].merge(v[2]).merge(v[3]).merge(v[4])
+    train_2011.to_netcdf('{}/2011/train.{}.nc'.format(root_dir, k))
+
+    train_2012 = v[0].merge(v[2]).merge(v[3]).merge(v[4])
+    train_2012.to_netcdf('{}/2012/train.{}.nc'.format(root_dir,k))
+
+    train_2013 = v[0].merge(v[1]).merge(v[3]).merge(v[4])
+    train_2013.to_netcdf('{}/2013/train.{}.nc'.format(root_dir, k))
+
+    train_2014 = v[0].merge(v[1]).merge(v[2]).merge(v[4])
+    train_2014.to_netcdf('{}/2014/train.{}.nc'.format(root_dir, k))
+
+    train_2015 = v[0].merge(v[1]).merge(v[2]).merge(v[3])
+    train_2015.to_netcdf('{}/2015/train.{}.nc'.format(root_dir, k))
--- research/kelsey/run_rf.py
+++ research/kelsey/run_rf.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-@author: kdoerksen - refactored from marchett for local use
+@author: kdoerksen
 Runs simple RF model without having to run full
 ML pipeline. Good for quick experiments.
 """
 
 import numpy as np
+from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
+import os
 import logging
 from sklearn.ensemble import RandomForestRegressor
 import xarray as xr
@@ -21,9 +22,10 @@ from sklearn.metrics    import (
     mean_squared_error,
     r2_score
 )
+from joblib import dump
+from sudsaq.utils import align_print
+from sklearn.inspection import permutation_importance
 from scipy.stats import pearsonr
-import cartopy.crs as ccrs
-import seaborn as sns
 
 parser = argparse.ArgumentParser(description='Local RF Run')
 parser.add_argument("--analysis_year", help="Specify year to use for test, all other years used for training."
@@ -32,6 +34,9 @@ parser.add_argument("--region", help="Boundary region on Earth to take data. Mus
                                      "globe, europe, asia, australia, north_america, west_europe, "
                                      "east_europe, west_na, east_na.")
 parser.add_argument("--month", help="Month of analysis. Currently only support jan, jul")
+parser.add_argument("--add_gee", help="Specify whether to incorporate gee datasets with y")
+parser.add_argument("--parameter_tuning", help="Specify whether to incorporate param tuning with y")
+
 args = parser.parse_args()
 
 script_home = '/Users/kelseydoerksen/code/suds-air-quality/research/kelsey/run_rf.py'
@@ -55,6 +60,7 @@ def format_lon(x):
 
 def format_gee(gee, train_x):
     '''
+    OLD, don't need anymore
     Formatting gee added data to match
     training data dim order, time
     '''
@@ -70,7 +76,7 @@ def format_gee(gee, train_x):
 
     return gee_final
 
-def generate_X_and_y(X_train, y_train, modis, fire, pop):
+def generate_X_and_y(X_train, y_train, modis, fire, pop, gee):
     '''
     Generates X for input, y for target
     for RF fit
@@ -80,9 +86,8 @@ def generate_X_and_y(X_train, y_train, modis, fire, pop):
 
     # Filter data by lat-lon bounds specific in bbox
     toar_ds = format_lon(toar_ds)
-    toar_ds = toar_ds.sel(lat=slice(bbox[2], bbox[3]), lon=slice(bbox[0], bbox[1]))
-
-    y_t = toar_ds['target'].values
+    y_toar = toar_ds.sel(lat=slice(bbox[2], bbox[3]), lon=slice(bbox[0], bbox[1]))
+    y_t = y_toar['target'].values
 
     # Remove NaNs
     mask_training = ~np.isnan(y_t)
@@ -90,44 +95,98 @@ def generate_X_and_y(X_train, y_train, modis, fire, pop):
 
     # Read in training data
     data = xr.open_dataset(X_train)
+    data = format_lon(data)
 
-    # Dealing with gee
-    modis_data = xr.open_dataset(modis)
-    fire_data = xr.open_dataset(fire)
-    pop_data = xr.open_dataset(pop)
-    combined_data = data.merge(modis_data).merge(fire_data).merge(pop_data)
+    if gee:
+        # Dealing with gee
+        modis_data = xr.open_dataset(modis)
+        #fire_data = xr.open_dataset(fire)
+        pop_data = xr.open_dataset(pop)
+        #combined_data = data.merge(modis_data).merge(fire_data).merge(pop_data)
+        combined_data = data.merge(modis_data).merge(pop_data)
+    else:
+        combined_data = data
 
     # Filter data by lat-lon bounds specific in bbox
-    data = format_lon(combined_data)
-    data = data.sel(lat=slice(bbox[2], bbox[3]), lon=slice(bbox[0], bbox[1]))
+    combined_data = combined_data.sel(lat=slice(bbox[2], bbox[3]), lon=slice(bbox[0], bbox[1]))
 
-    variable_names = list(data.keys())
+    variable_names = list(combined_data.keys())
     x = []
     for j in tqdm(range(len(variable_names))):
-        x_data = data[variable_names[j]].values[mask_training]
+        x_data = combined_data[variable_names[j]].values[mask_training]
         x.append(x_data)
     x_t = np.column_stack(x)
 
     return x_t, y_t
 
-def calc_importances(model, feature_names):
+def calc_importances(model, feature_names, dir):
     '''
     Calculate feature importances, save as txt
     and plot
     '''
     importances = model.feature_importances_
-    fi_labeled = pd.Series(importances, index=feature_names)
-    # sort by highest to lowest
-    fi_labeled = fi_labeled.sort_values(ascending=False)
-    print(fi_labeled)
-    fi_labeled.plot.bar()
-    plt.title('Feature Importance')
+    stddev = np.std([est.feature_importances_ for est in model.estimators_], axis=0)
+    df = pd.DataFrame(np.array([importances, stddev]), columns=feature_names, index=['importance', 'stddev'])
+    df = df.sort_values(by='importance', axis=1, ascending=False)
+    fmt = {}
+    for var, vals in df.items():
+        fmt[var] = f'{vals.importance} +/- {vals.stddev}'
+
+    Logger.info('Permutation importance +/- stddev:')
+    strings = align_print(fmt, enum=True, print=Logger.info)
+    with open('{}/importances.txt'.format(dir), 'w') as file:
+        file.write('\n\nFeature Importance:\n')
+        file.write('\n'.join(strings))
+
+    return df
+
+def calc_perm_importance(model, data, target, feature_names, dir):
+    permimp = permutation_importance(model, data, target, random_state=0)
+    # Only want the summaries, remove the importances array
+    del permimp['importances']
+
+    # Convert to a DataFrame and sort by importance value
+    df = pd.DataFrame(permimp.values(), columns=feature_names, index=['importance', 'stddev'])
+    df = df.sort_values(by='importance', axis=1, ascending=False)
+    fmt = {}
+    for var, vals in df.items():
+        fmt[var] = f'{vals.importance} +/- {vals.stddev}'
+
+    Logger.info('Permutation importance +/- stddev:')
+    strings = align_print(fmt, enum=True, print=Logger.info)
+    with open('{}/perm_importances.txt'.format(dir), 'w') as file:
+        file.write('\n\nPermutation Feature Importance:\n')
+        file.write('\n'.join(strings))
+
+    return df
+
+def plot_importances(imp, perm, dir, month, year, region):
+    '''
+    Generates bar plot for FI with perm
+    '''
+
+    # Normalize first
+    imp = imp / imp.max(axis=1).importance
+    perm = perm / perm.max(axis=1).importance
+
+    # Plot only top 15 features
+    imp_top = imp[imp.columns[:20]]
+    perm_top = perm[perm.columns[:20]]
+    X_axis = np.arange(20)
+    plt.bar(X_axis - 0.2, imp_top.loc['importance'].values, yerr=imp_top.loc['stddev'].values,
+            width=0.4, label='Importance')
+    plt.bar(X_axis + 0.2, perm_top.loc['importance'].values, yerr=perm_top.loc['stddev'].values,
+            width=0.4, label='Permutation Importance')
+    plt.xticks(X_axis, imp_top, rotation=90)
+    plt.title('Feature Importances for Testing set {} {} {}'.format(month, year, region))
     plt.xlabel('Feature Name')
     plt.ylabel('Importance')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig('{}/importances.png'.format(dir))
     plt.show()
-    plt.savefig('testing.png')
 
-def truth_vs_predicted(target, predict, label=None, save=None):
+def truth_vs_predicted(target, predict, dir, region):
     """
     """
     fig, ax = plt.subplots(figsize=(10, 10))
@@ -145,7 +204,7 @@ def truth_vs_predicted(target, predict, label=None, save=None):
     kernel = stats.gaussian_kde([target, predict])
     density = kernel([target, predict])
 
-    plot = ax.scatter(target, predict, c=density, cmap='viridis', label=label, s=5)
+    plot = ax.scatter(target, predict, c=density, cmap='viridis', s=5)
 
     # Create the colorbar without ticks
     cbar = fig.colorbar(plot, ax=ax)
@@ -155,24 +214,27 @@ def truth_vs_predicted(target, predict, label=None, save=None):
     cbar.set_label('Density')
     ax.set_xlabel('Truth')
     ax.set_ylabel('Predicted')
-    ax.set_title('Truth vs Predicted')
-
-    if label:
-        legend = ax.legend(handlelength=0, handletextpad=0, loc='upper left', prop={'family': 'monospace'})
-        legend.legendHandles[0].set_visible(False)
+    ax.set_title('Truth vs Predicted for {}'.format(region))
 
     plt.axis('equal')
     plt.tight_layout()
-    if save:
-        Logger.info(f'Saving truth_vs_predicted plot to {save}')
-        plt.savefig(save)
+    plt.savefig('{}/truth_vs_pred.png'.format(dir))
     plt.show()
 
-def plot_true_vs_predicted_map():
+def plot_histogram(target, pred, dir, region):
     '''
-    Generates map plot of true vs predicted bias
+    Plot histogram of true vs predicted
     '''
-
+    bins = np.linspace(-60, 60, 100)
+    plt.hist(target, bins, histtype='step', label=['target'])
+    plt.hist(pred, bins, histtype='step', label=['prediction'])
+    plt.legend(loc='upper right')
+    plt.tight_layout()
+    plt.xlabel('bias')
+    plt.ylabel('count')
+    plt.title('Truth vs Predicted Histogram for {}'.format(region))
+    plt.savefig('{}/truth_vs_pred_hist.png'.format(dir))
+    plt.show()
 
 # Set boundaries to analyze by region
 bbox_dict = {'globe':[-180, 180, -90, 90],
@@ -191,55 +253,107 @@ bbox = bbox_dict[aoi]
 
 analysis_year = int(args.analysis_year)
 month = args.month
+add_gee = args.add_gee
+param_tuning = args.parameter_tuning
+
+if not add_gee:
+    results_dir = '/Users/kelseydoerksen/exp_runs/rf/{}/all_gee_added/{}/results_no_gee/{}/test'.\
+        format(month, analysis_year, aoi)
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
+else:
+    results_dir = '/Users/kelseydoerksen/exp_runs/rf/{}/all_gee_added/{}/results/{}/test'.\
+        format(month, analysis_year, aoi)
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
 
 Logger.debug('Grabbing data from month {}'.format(month))
 
+# --- Below is a total mess and I need to clean it up but works for now ---
+
 # Dealing with training data
-x_train = f'{root_dir}/{month}/{analysis_year}/train.data.nc'
-y_train = f'{root_dir}/{month}/{analysis_year}/train.target.nc'
-
-modis_train = f'{root_dir}/{month}/{analysis_year}/train.modis.nc'
-fire_train = f'{root_dir}/{month}/{analysis_year}/train.fire.nc'
-pop_train = f'{root_dir}/{month}/{analysis_year}/train.pop.nc'
-
-X, y = generate_X_and_y(x_train, y_train, modis_train, fire_train, pop_train)
+x_train = f'{root_dir}/{month}/all_gee_added/{analysis_year}/train.data.nc'
+y_train = f'{root_dir}/{month}/all_gee_added/{analysis_year}/train.target.nc'
 
 # --- Dealing with testing data ---
-testing_x = f'{root_dir}/{month}/{analysis_year}/test.data.nc'
-testing_y = f'{root_dir}/{month}/{analysis_year}/test.target.nc'
-modis_test = f'{root_dir}/{month}/{analysis_year}/test.modis.nc'
-fire_test = f'{root_dir}/{month}/{analysis_year}/test.fire.nc'
-pop_test = f'{root_dir}/{month}/{analysis_year}/test.pop.nc'
+testing_x = f'{root_dir}/{month}/all_gee_added/{analysis_year}/test.data.nc'
+testing_y = f'{root_dir}/{month}/all_gee_added/{analysis_year}/test.target.nc'
 
 # Read in toar testing target
 toar_test = xr.open_dataset(testing_y)
-y_test = toar_test['target'].values
+toar_test = format_lon(toar_test)
+y = toar_test.sel(lat=slice(bbox[2], bbox[3]), lon=slice(bbox[0], bbox[1]))
+y_test = y['target'].values
 # Remove Nans
 mask_testing = ~np.isnan(y_test)
 y_test = y_test[mask_testing]
 
 data_test = xr.open_dataset(testing_x)
-modis_ds = xr.open_dataset(modis_test)
-fire_ds = xr.open_dataset(fire_test)
-pop_ds = xr.open_dataset(pop_test)
+data_test = format_lon(data_test)
 
-# Read in gee datasets to add to training data
-combined_test = data_test.merge(modis_ds).merge(fire_ds).merge(pop_ds)
+if add_gee:
+    modis_train = f'{root_dir}/{month}/all_gee_added/{analysis_year}/train.modis.nc'
+    #fire_train = f'{root_dir}/{month}/{analysis_year}/train.fire.nc'
+    fire_train = None
+    pop_train = f'{root_dir}/{month}/all_gee_added/{analysis_year}/train.pop.nc'
 
+    # Deal with gee testing data
+    modis_test = f'{root_dir}/{month}/all_gee_added/{analysis_year}/test.modis.nc'
+    #fire_test = f'{root_dir}/{month}/{analysis_year}/test.fire.nc'
+    pop_test = f'{root_dir}/{month}/all_gee_added/{analysis_year}/test.pop.nc'
+    modis_ds_test = xr.open_dataset(modis_test)
+    #fire_ds_test = xr.open_dataset(fire_test)
+    pop_ds_test = xr.open_dataset(pop_test)
+    #combined_test = data_test.merge(modis_ds_test).merge(fire_ds_test).merge(pop_ds_test)
+    combined_test = data_test.merge(modis_ds_test).merge(pop_ds_test)
+else:
+    modis_train, fire_train, pop_train = None, None, None
+    add_gee = False
+    combined_test = data_test
+
+# Deal with training data
+X, y = generate_X_and_y(x_train, y_train, modis_train, fire_train, pop_train, add_gee)
 
 var_names = list(combined_test.keys())
+combined_test = combined_test.sel(lat=slice(bbox[2], bbox[3]), lon=slice(bbox[0], bbox[1]))
 X_test = []
 for j in tqdm(range(len(var_names))):
     x = combined_test[var_names[j]].values[mask_testing]
     X_test.append(x)
 X_test = np.column_stack(X_test)
 
-Logger.info('Running Random Forest')
-rf = RandomForestRegressor(n_estimators=20,
-                           max_features=int(len(var_names) * 0.3),
-                           random_state=6789,
-                           verbose=1)
+if param_tuning:
+    Logger.info('Finding optimal hyperparameters')
+    params = [{
+                'n_estimators': [50, 100, 150, 200],
+                'max_depth': [3, 4, 5]
+            }]
+    kfold = KFold(n_splits=5, shuffle=True, random_state=1234)
+    clf = GridSearchCV(
+        RandomForestRegressor(), params, cv=kfold,
+        scoring='neg_root_mean_squared_error', n_jobs=10, error_score='raise'
+    )
+    clf.fit(X, y)
+    print('Parameter selection:')
+    print(f'n_estimators: {clf.best_params_["n_estimators"]}')
+    print(f'max_depth: {clf.best_params_["max_depth"]}')
+
+    # Create the Gradient Boosting predictor
+    Logger.info('Training Random Forest')
+    rf = RandomForestRegressor(n_estimators=clf.best_params_['n_estimators'],
+                               max_depth=clf.best_params_['max_depth'],
+                               random_state=300,
+                               verbose=1)
+else:
+    rf = RandomForestRegressor(n_estimators=100,
+                               max_features=int(0.3*(len(var_names))),
+                               random_state=300,
+                               verbose=1)
+
 rf.fit(X, y)
+out_model = os.path.join(results_dir, 'random_forest_predictor.joblib')
+dump(rf, out_model)
+
 yhat = rf.predict(X_test)
 
 # Calculate rmse
@@ -259,9 +373,24 @@ print("r correlation is: {}".format(r))
 r2 = r2_score(y_test,yhat)
 print('r2 score is: {}'.format(r2))
 
-# Calculate, plot, and save importances
-calc_importances(rf, var_names)
+with open('{}/results.txt'.format(results_dir), 'w') as f:
+    f.write(' rmse is {}'.format(rmse))
+    f.write(' mean absolute percentage error is: {}'.format(mape))
+    f.write(" r correlation is: {}".format(r))
+    f.write(' r2 score is: {}'.format(r2))
+
+# Calculate importances
+importances = calc_importances(rf, var_names, results_dir)
+
+# Calculate permutation importance
+perm_importances = calc_perm_importance(rf, X_test, y_test, var_names, results_dir)
+
+# Plot importances
+plot_importances(importances, perm_importances, results_dir, month, analysis_year, aoi)
 
 # Plot true vs predicted
 Logger.info('Plotting y_test vs y_hat')
-truth_vs_predicted(y_test, yhat)
+truth_vs_predicted(y_test, yhat, results_dir, aoi)
+
+# Plot histogram
+plot_histogram(y_test, yhat, results_dir, aoi)
