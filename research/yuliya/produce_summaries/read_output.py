e6f96d7f669e6fdc5e86db0b681b44c4725e3dcf
YM <yuliya.marchetti@jpl.nasa.gov>
2023-01-05 14:13:48 -0800

    change importance reader

--- research/yuliya/produce_summaries/read_output.py
+++ research/yuliya/produce_summaries/read_output.py
@@ -36,85 +36,86 @@ if not os.path.exists(root_dir):
 
 
 #--------- LOAD importances
-def load_importances(models_dir):
-    output_files = glob.glob(f'{models_dir}/*/*/*importance.h5')
+# def load_importances(models_dir):
+#     output_files = glob.glob(f'{models_dir}/*/*/*importance.h5')
     
-    mi = []
-    pi = []
-    mi_names = []
-    pi_names = []
-    pi_months = []
-    for file in output_files:
-        try:
-            with closing(h5py.File(file, 'r')) as f:
-                mi.append(f['model']['block0_values'][0,:])
-                mi_names.append(f['model']['axis0'][:].astype(str))
+#     mi = []
+#     pi = []
+#     mi_names = []
+#     pi_names = []
+#     pi_months = []
+#     for file in output_files:
+#         try:
+#             with closing(h5py.File(file, 'r')) as f:
+#                 mi.append(f['model']['block0_values'][0,:])
+#                 mi_names.append(f['model']['axis0'][:].astype(str))
                 
-                pi.append(f['permutation']['block0_values'][0, :])
-                pi_names.append(f['permutation']['axis0'][:].astype(str))
-                pi_months.append(file.split('/')[-3])
-        except:
-            mi_months[-1] = np.nan
-            pi_months[-1] = np.nan
-            continue
+#                 pi.append(f['permutation']['block0_values'][0, :])
+#                 pi_names.append(f['permutation']['axis0'][:].astype(str))
+#                 pi_months.append(file.split('/')[-3])
+#         except:
+#             mi_months[-1] = np.nan
+#             pi_months[-1] = np.nan
+#             continue
     
-    #sort everything to the first model importance order
-    reference_labels = mi_names[0]
-    tick_labels = np.hstack(['.'.join(x.split('.')[1:]) for x in reference_labels])
-    tick_labels = np.hstack([x.split('2dsfc')[-1].split('.')[-1] for x in tick_labels])
+#     #sort everything to the first model importance order
+#     reference_labels = mi_names[0]
+#     tick_labels = np.hstack(['.'.join(x.split('.')[1:]) for x in reference_labels])
+#     tick_labels = np.hstack([x.split('2dsfc')[-1].split('.')[-1] for x in tick_labels])
     
-    #sort each importance vector by name
-    mi_sorted = np.zeros((len(reference_labels), len(mi)))
-    pi_sorted = np.zeros((len(reference_labels), len(pi)))
-    for i in range(len(mi)):
-        mi_idx = np.hstack([np.where(mi_names[i] == label)[0] for label in reference_labels])
-        mi_sorted[:, i] = mi[i][mi_idx] / mi[i][mi_idx].max()
+#     #sort each importance vector by name
+#     mi_sorted = np.zeros((len(reference_labels), len(mi)))
+#     pi_sorted = np.zeros((len(reference_labels), len(pi)))
+#     for i in range(len(mi)):
+#         mi_idx = np.hstack([np.where(mi_names[i] == label)[0] for label in reference_labels])
+#         mi_sorted[:, i] = mi[i][mi_idx] / mi[i][mi_idx].max()
         
-        pi_idx = np.hstack([np.where(pi_names[i] == label)[0] for label in reference_labels])
-        pi_sorted[:, i] = pi[i][pi_idx] / pi[i][pi_idx].max()
+#         pi_idx = np.hstack([np.where(pi_names[i] == label)[0] for label in reference_labels])
+#         pi_sorted[:, i] = pi[i][pi_idx] / pi[i][pi_idx].max()
               
-    months_list = np.hstack([x.split('/')[-3] for x in output_files])
-    mi_monthly_mean = np.zeros((len(reference_labels), len(plots.MONTHS)))
-    mi_monthly_std = np.zeros((len(reference_labels), len(plots.MONTHS)))
-    pi_monthly_mean = np.zeros((len(reference_labels), len(plots.MONTHS)))
-    pi_monthly_std = np.zeros((len(reference_labels), len(plots.MONTHS)))
-    for m in range(len(plots.MONTHS)):
-        mask_month = months_list == plots.MONTHS[m]
-        if mask_month.sum() > 0:
-            mi_monthly_mean[:, m] = mi_sorted[:, mask_month].mean(axis = 1)
-            mi_monthly_std[:, m] = mi_sorted[:, mask_month].std(axis = 1)
-            pi_monthly_mean[:, m] = pi_sorted[:, mask_month].mean(axis = 1)
-            pi_monthly_std[:, m] = pi_sorted[:, mask_month].std(axis = 1)
-        else:
-            mi_monthly_mean[:, m] = np.repeat(np.nan, len(reference_labels))
-            mi_monthly_std[:, m] = np.repeat(np.nan, len(reference_labels))
-            pi_monthly_mean[:, m] = np.repeat(np.nan, len(reference_labels))
-            pi_monthly_std[:, m] = np.repeat(np.nan, len(reference_labels))
+#     months_list = np.hstack([x.split('/')[-3] for x in output_files])
+#     mi_monthly_mean = np.zeros((len(reference_labels), len(plots.MONTHS)))
+#     mi_monthly_std = np.zeros((len(reference_labels), len(plots.MONTHS)))
+#     pi_monthly_mean = np.zeros((len(reference_labels), len(plots.MONTHS)))
+#     pi_monthly_std = np.zeros((len(reference_labels), len(plots.MONTHS)))
+#     for m in range(len(plots.MONTHS)):
+#         mask_month = months_list == plots.MONTHS[m]
+#         if mask_month.sum() > 0:
+#             mi_monthly_mean[:, m] = mi_sorted[:, mask_month].mean(axis = 1)
+#             mi_monthly_std[:, m] = mi_sorted[:, mask_month].std(axis = 1)
+#             pi_monthly_mean[:, m] = pi_sorted[:, mask_month].mean(axis = 1)
+#             pi_monthly_std[:, m] = pi_sorted[:, mask_month].std(axis = 1)
+#         else:
+#             mi_monthly_mean[:, m] = np.repeat(np.nan, len(reference_labels))
+#             mi_monthly_std[:, m] = np.repeat(np.nan, len(reference_labels))
+#             pi_monthly_mean[:, m] = np.repeat(np.nan, len(reference_labels))
+#             pi_monthly_std[:, m] = np.repeat(np.nan, len(reference_labels))
     
    
-    importances = {'model': 
-                   {'norm': mi_sorted, 
-                    'monthly_mean': mi_monthly_mean,
-                    'mean': np.nanmean(mi_monthly_mean, axis = 1), 
-                    'monthly_std': mi_monthly_std,
-                    'std': np.nanmean(mi_monthly_std, axis = 1)}, 
-                   'permutation': 
-                       {'norm': pi_sorted, 
-                        'monthly_mean': pi_monthly_mean, 
-                        'mean': np.nanmean(pi_monthly_mean, axis = 1),
-                        'monthly_std': pi_monthly_std,
-                        'std': np.nanmean(pi_monthly_std, axis = 1)},
-                   'months_list': months_list,
-                   'labels': tick_labels,
-                   'reference': reference_labels}
+#     importances = {'model': 
+#                    {'norm': mi_sorted, 
+#                     'monthly_mean': mi_monthly_mean,
+#                     'mean': np.nanmean(mi_monthly_mean, axis = 1), 
+#                     'monthly_std': mi_monthly_std,
+#                     'std': np.nanmean(mi_monthly_std, axis = 1)}, 
+#                    'permutation': 
+#                        {'norm': pi_sorted, 
+#                         'monthly_mean': pi_monthly_mean, 
+#                         'mean': np.nanmean(pi_monthly_mean, axis = 1),
+#                         'monthly_std': pi_monthly_std,
+#                         'std': np.nanmean(pi_monthly_std, axis = 1)},
+#                    'months_list': months_list,
+#                    'labels': tick_labels,
+#                    'reference': reference_labels}
     
-    return importances
+#     return importances
 
 
 
 #--------- LOAD importances
 def load_importances(summaries_dir):
-    output_files = glob.glob(f'{summaries_dir}/*/*/*importance.h5')
+    
+    output_files = glob.glob(f'{summaries_dir}/*/*importances*.h5')
     
     mi = []
     pi = []
@@ -122,35 +123,49 @@ def load_importances(summaries_dir):
     pi_names = []
     pi_months = []
     for file in output_files:
-        try:
-            with closing(h5py.File(file, 'r')) as f:
-                mi.append(f['model']['values'])
-                mi_names.append(f['model']['axis0'][:].astype(str))
-                
-                pi.append(f['permutation']['values'])
-                pi_names.append(f['permutation']['axis0'][:].astype(str))
-                pi_months.append(file.split('/')[-3])
-        except:
-            mi_months[-1] = np.nan
-            pi_months[-1] = np.nan
-            continue
+        with closing(h5py.File(file, 'r')) as f:
+            mi.append(f['model']['values'][:])
+            mi_names.append(f['model']['names'][:].astype(str))
+            
+            pi.append(f['permutation']['values'][:])
+            pi_names.append(f['permutation']['names'][:].astype(str))
+            
+            p = pi[-1].shape[1]
+            pi_months.append(np.repeat(file.split('/')[-2], p))
     
     #sort everything to the first model importance order
-    reference_labels = mi_names[0]
+    reference_labels = mi_names[0][:, 0]
     tick_labels = np.hstack(['.'.join(x.split('.')[1:]) for x in reference_labels])
     tick_labels = np.hstack([x.split('2dsfc')[-1].split('.')[-1] for x in tick_labels])
     
-    #sort each importance vector by name
-    mi_sorted = np.zeros((len(reference_labels), len(mi)))
-    pi_sorted = np.zeros((len(reference_labels), len(pi)))
-    for i in range(len(mi)):
-        mi_idx = np.hstack([np.where(mi_names[i] == label)[0] for label in reference_labels])
-        mi_sorted[:, i] = mi[i][mi_idx] / mi[i][mi_idx].max()
+    mi = np.column_stack(mi)
+    pi = np.column_stack(pi)
+    mi_names = np.column_stack(mi_names)
+    pi_names = np.column_stack(pi_names)
+    mi_sorted = mi.copy(); pi_sorted = pi.copy()
+    for i in range(mi.shape[1]):
+        mi_idx = np.hstack([np.where(mi_names[:, i] == label)[0] 
+                            for label in reference_labels])
+        mi_sorted[:, i] = mi[mi_idx, i] / mi[mi_idx, i].max()
         
-        pi_idx = np.hstack([np.where(pi_names[i] == label)[0] for label in reference_labels])
-        pi_sorted[:, i] = pi[i][pi_idx] / pi[i][pi_idx].max()
-              
-    months_list = np.hstack([x.split('/')[-3] for x in output_files])
+        pi_idx = np.hstack([np.where(pi_names[:, i] == label)[0] 
+                            for label in reference_labels])
+        pi_sorted[:, i] = pi[pi_idx, i] / pi[pi_idx, i].max()
+    
+    
+    #sort each importance vector by name
+    # mi_sorted = []
+    # pi_sorted = np.zeros((len(reference_labels), len(pi)))
+    # for i in range(len(mi)):
+        
+    #     mi_idx = np.hstack([np.where(mi_names[i] == label)[0] for label in reference_labels])
+    #     mi_sorted.append(mi[i][mi_idx] / mi[i][mi_idx].max(axis = 0))
+        
+    #     pi_idx = np.hstack([np.where(pi_names[i] == label)[0] for label in reference_labels])
+    #     pi_sorted[:, i] = pi[i][pi_idx] / pi[i][pi_idx].max()
+    
+    months_list = np.hstack(pi_months)          
+    #months_list = np.hstack([x.split('/')[-2] for x in output_files])
     mi_monthly_mean = np.zeros((len(reference_labels), len(plots.MONTHS)))
     mi_monthly_std = np.zeros((len(reference_labels), len(plots.MONTHS)))
     pi_monthly_mean = np.zeros((len(reference_labels), len(plots.MONTHS)))
